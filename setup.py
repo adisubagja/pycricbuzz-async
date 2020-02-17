ba951fda97578acb1eb02463d14417d70ac6e544
Skelmis <47520067+Skelmis@users.noreply.github.com>
2020-02-17 16:28:58 +1300

    Add files via upload
    .

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 setup(
   name = 'pycricbuzz',
-  packages = ['pycricbuzz'], 
+  packages = ['pycricbuzz'],
   version = '2.4',
   description = 'A library for fetching live cricket scores from cricbuzz',
   author = 'Shivam Mitra',
   author_email = 'shivamm389@gmail.com',
   license = 'GPLv2',
-  url = 'https://github.com/codophobia/pycricbuzz', 
-  download_url = 'https://github.com/codophobia/pycricbuzz/tarball/2.4', 
-  keywords = ['cricket', 'cricbuzz'], 
+  url = 'https://github.com/codophobia/pycricbuzz',
+  download_url = 'https://github.com/codophobia/pycricbuzz/tarball/2.4',
+  keywords = ['cricket', 'cricbuzz'],
   install_requires=[
-          'requests',
+          'aiohttp',
       ],
   classifiers = [],
 )
